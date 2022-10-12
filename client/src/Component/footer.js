799938ab648e89fe2d1811f04445a0cd4b530607
Hemanthreddybollavaram <hemanthkumarreddybollavaram@gmail.com>
2022-10-12 19:57:29 +0530

    initial commit

---
+++ client/src/Component/footer.js
@@ -0,0 +1,20 @@
+import './mystyle.css'
+export default function footer() {
+  return (
+    <div class="container-fluid bg1">
+      <footer class="site-footer">
+        
+        <h5 class="footer-style">
+          Review My Page.com | Developed by&nbsp;
+          <a 
+            class="text-warning text-decoration-none"
+            href="https://www.linkedin.com/in/bollavaram-hemanth-kumar-reddy-72593320a/"
+            target="_blank"
+          >
+           Bollavaram Hemanth Kumar Reddy
+          </a>
+        </h5>
+      </footer>
+    </div>
+  );
+}
